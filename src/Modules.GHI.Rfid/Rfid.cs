b96740ea16da1f86af454469e45749a8061b079b
xas <xas@users.noreply.github.com>
2020-03-24 01:00:58 +0100

    Add RFID module project
    * Preview code with event missing

---
+++ src/Modules.GHI.Rfid/Rfid.cs
@@ -0,0 +1,28 @@
+﻿using Nanoteer.Core;
+using Nanoteer.Core.Serial;
+using System;
+
+namespace Nanoteer.Modules.GHI
+{
+    public class Rfid
+    {
+        private SerialSocket _serial;
+
+        public Rfid(int socketNumber)
+        {
+            Socket socket = Socket.AttachToSocket(socketNumber, SocketType.U);
+            _serial = new SerialSocket(socket, (char)3);
+            _serial.SerialBytesRead += SerialBytesRead;
+        }
+
+        private void SerialBytesRead(SerialSocket serial, byte[] dataRead)
+        {
+            Console.Write("Bytes: >>");
+            for (int i = 0; i < dataRead.Length; i++)
+            {
+                Console.Write(dataRead[i] + " ");
+            }
+            Console.WriteLine("<< ");
+        }
+    }
+}
